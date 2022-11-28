d97d9e3ecdb04b06aa499d0e7b361156f6bd6b54
tombjorn <118115159+tombjorn@users.noreply.github.com>
2022-11-28 20:52:07 +0000

    Create PointersCont.c

---
+++ PointersCont.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+int main(void)
+{
+    int x = 1;
+    int y = 2;
+    printf("x = %i, y = %i\n", x, y);
+
+    swap(&x, &y);
+
+    printf("x = %i, y = %i\n", x, y);
+
+
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+
+    *a = *b;
+    *b = temp;
+
+    printf("a = %i, b = %i\n", *a, *b);
+
+}
